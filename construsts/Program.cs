bff8e15f4e316b1455507bc266d3c77ba048f5c5
Giorf81 <rejroberts7@gmail.com>
2024-02-26 23:02:30 +0300

    Добавьте файлы проекта.

---
+++ construsts/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace construsts
+{
+
+    public class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen() 
+        {
+            color = "Black";
+            cost = 100;
+        }
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+
+
+
+        
+    }
+    
+}
+
